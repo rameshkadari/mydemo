9879d1d11c53909fcdc4b9739216c1cc68e799b2
rames <rames@LAPTOP-35VMJECB>
2018-04-15 09:53:32 +0530

    my changes

--- mydemogit/src/com/mydemogit/controller/ContactController.java
+++ mydemogit/src/com/mydemogit/controller/ContactController.java
@@ -1,6 +1,6 @@
 package com.mydemogit.controller;
 
-public class DemoController {
+public class ContactController {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
